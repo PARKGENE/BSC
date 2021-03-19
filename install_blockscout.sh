a441e787fb7102efdb60daeb7ced9e2a651e886b
costis papadim <costispapadim@gmail.com>
2021-03-19 19:39:01 +0200

    asdasc

--- install_blockscout.sh
+++ install_blockscout.sh
@@ -58,7 +58,7 @@ sudo apt-get install -y git
 sudo git clone https://github.com/poanetwork/blockscout.git && echo "cloned"
 # sudo git clone https://github.com/PARKGENE/BSC.git && echo "cloned"
 cd blockscout
-sudo git checkout 53ea60c3 && echo "checked out"; cd -
+#sudo git checkout 53ea60c3 && echo "checked out"; cd -
 #sudo git checkout e9f57aa && echo "checked out"; cd -
 # sudo chmod -R a+x blockscout && echo "permissions granted"
 
